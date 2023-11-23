6875d2de912f97d15c52c01793946254ccd9fac1
fly <1785613048@qq.com>
2023-11-23 09:37:16 +0800

    Initial commit

---
+++ src/test/java/com/backend/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
