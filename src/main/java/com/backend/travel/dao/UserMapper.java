d893f421621395ed23e5a10cb6641e8edd48b0d5
fly <1785613048@qq.com>
2023-11-26 17:28:52 +0800

    添加用户信息

---
+++ src/main/java/com/backend/travel/dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.backend.travel.dao;
+
+import com.backend.travel.POJO.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author admin
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2023-11-26 11:03:08
+* @Entity com.backend.travel.POJO.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
